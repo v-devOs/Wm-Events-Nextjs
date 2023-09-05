3e3792948ce199072895b1ed44fb52dc7735d9fd
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-09-05 07:24:45 -0600

    Preparando muda a firebase

--- database/index.ts
+++
@@ -1,2 +0,0 @@
-export *  as database from './database'
-export * as codes from './codes'
--- pages/api/codes/index.ts
+++ pages/api/codes/index.ts
@@ -1,6 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { ICode } from '@/interfaces'
-import { codes } from '@/database'
 
 type Data = { message: string } | ICode[]
 
@@ -24,45 +23,22 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
 
 const getCodesFromDatabase = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
   
-  const data = await codes.getCodes() as ICode[] 
-
-  return data
-    ? res.status(200).json( data )
-    : res.status(400).json({ message: 'Error al obtener los codigos' })
+  
 }
 
 
 const setCodesInDatabase = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
 
-  const { numberOfCodesToCreate } = req.body as { numberOfCodesToCreate: number}
-  const codesToSave = await codes.createCodes( numberOfCodesToCreate )
-
-  if( !codesToSave ) return res.status(400).json({message: 'Error al generar los codigos'})
-
-  const codesSaved = await codes.saveCodes( codesToSave )
-
-  return codesSaved
-    ? res.status(200).json( codesSaved )
-    : res.status(400).json({ message: 'Error al guardar los codigos' })
+  
 }
 
 
 const updateCodeInDatabse = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
   
-  const { code, name } = req.body as { code: string, name: string }
-
-  const resp = await codes.updateCode( code, name )
-
-  return resp
-    ? res.status(200).json({ message: 'Codigo actualizado correctamente' })
-    : res.status(400).json({ message: 'Error al actualizar codigo' })
+  
 }
 
 const deleteCodesFromDatabase = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
   
-  const resp = await codes.deleteAllCodes()
-
-  return resp
-   ? res.status(200).json({ message: 'Codigos eliminados correctamente' })
-   : res.status(500).json({ message: 'Error del servidor al eliminar los codigos'})
+  
 }
